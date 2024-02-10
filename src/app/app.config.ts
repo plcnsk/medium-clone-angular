5541439315904394611277ff8f480cdc67e254b3
Aliaksei Pliutsinski <aleksprutik@gmail.com>
2024-02-10 19:30:58 +0100

    feat: add auth reducer and auth state interface

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,18 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import { authReducer } from './auth/store/reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(),
     provideHttpClient(),
+    provideState({ name: 'auth', reducer: authReducer }),
     provideStoreDevtools({ maxAge: 50, logOnly: !isDevMode() }),
   ],
 };
