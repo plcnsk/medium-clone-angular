32f314ab1257c8bb5f4c22c36a28180ba2bd3560
Aliaksei Pliutsinski <aleksprutik@gmail.com>
2024-02-12 22:07:07 +0100

    feat: add beckend errors interface

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,10 +1,19 @@
 import { Action, createReducer, on } from '@ngrx/store';
 
 import { AuthStateInterface } from '../types/authState.interface';
-import { registerAction } from './actions/register.action';
+import {
+  registerAction,
+  registerFailureAction,
+  registerSuccessAction,
+} from './actions/register.action';
+import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
+import { BackendErrorsInterface } from '../../shared/types/backendErrors.interface';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
+  currentUser: null,
+  isLoggedIn: null,
+  validationErrors: null,
 };
 
 export const authReducer = createReducer(
@@ -14,6 +23,26 @@ export const authReducer = createReducer(
     (state): AuthStateInterface => ({
       ...state,
       isSubmitting: true,
+      validationErrors: null,
+    }),
+  ),
+
+  on(
+    registerSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: true,
+      currentUser: action.currentUser,
+    }),
+  ),
+
+  on(
+    registerFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors,
     }),
   ),
 );
